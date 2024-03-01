094e208bd80d636335cd93a24b1f8f52502198fb
Jose Ramon Taviro <1josegranada@gmail.com>
2024-03-01 18:06:27 +0100

    Vida y colisión
    Vida y colisión con el enemigo añadidos. Knockback e inmunidad para evitar bugs añadidos.

---
+++ Juego2D - Bloste/Assets/Script/EnemyScript/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public string enemyName;
+    public float healthPoints;
+    public float speed;
+    public float knockbackForceX;
+    public float knockbackForceY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
